25e33ff06e0e6c960a204b5d246beb8063a234cb
tui r <ratanachai.svs@gmail.com>
2020-08-27 13:09:25 +0700

    login with jdbc

--- src/main/java/th/go/rd/atm/service/CustomerService.java
+++ src/main/java/th/go/rd/atm/service/CustomerService.java
@@ -2,38 +2,46 @@ package th.go.rd.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import th.go.rd.atm.data.CustomerRepository;
 import th.go.rd.atm.model.Customer;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerService {
 
-    private List<Customer> customers = new ArrayList<>();
+//        private List<Customer> customers = new ArrayList<>();
+    private CustomerRepository repository;
 
 //    @PostConstruct
 //    public void postConstruct(){
 //        customers = new ArrayList<>();
 //    }
 
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
     public void createCustomer(Customer customer){
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customers.add(customer);
+        repository.save(customer);
+//        customers.add(customer);
     }
 
     public List<Customer> getCustomers(){
-        return new ArrayList<>(customers);
+        return repository.findAll();
+//        return new ArrayList<>(customers);
     }
 
     public Customer findCustomer(int id) {
-        for (Customer customer : customers) {
-            if (customer.getId() == id)
-                return customer;
-        }
-        return null;
+        return repository.findById(id);
+//        for (Customer customer : customers) {
+//            if (customer.getId() == id)
+//                return customer;
+//        }
+//        return null;
     }
 
     public Customer checkPin(Customer inputCustomer) {
